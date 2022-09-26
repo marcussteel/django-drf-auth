6339951cc1368ee42d6d7e9202fed633f1676a0f
marcussteel <105747723+marcussteel@users.noreply.github.com>
2022-09-26 12:54:35 +0300

    Add files via upload

--- student_api/views.py
+++ student_api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import HttpResponse
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from .permissions import IsAdminorReadOnly, IsAddedByUserorReadOnly
 
 
 
@@ -12,6 +14,10 @@ def home(request):
 class StudentList(generics.ListCreateAPIView):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
+    permission_classes=[IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
        
@@ -19,3 +25,4 @@ class StudentList(generics.ListCreateAPIView):
 class StudentOperations(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
+    permission_classes=[IsAddedByUserorReadOnly]
