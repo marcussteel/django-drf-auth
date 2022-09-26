b99ada46b646aea198b382aa6271fbbb12001494
marcussteel <105747723+marcussteel@users.noreply.github.com>
2022-09-26 12:55:03 +0300

    some changes

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'student_api',
     'rest_framework',
+    'rest_framework.authtoken',
+    'user_api',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
